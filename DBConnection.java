88d384b75c0c6ceedaf7c445e48e47e2b95cb174
Kshitij verma <kshitijkumar2702@gmail.com>
2024-11-10 16:40:14 +0530

    DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -3,11 +3,11 @@
 public class ConnectJDBC
 { 
 // database1 is the name of database you created
-	private static final String url="jdbc:mysql://localhost:3306/:database1";
+	private static final String url="jdbc:mysql://localhost:3306/:list";
 	
 	// user and pwd is ur user name and password of oracle database
-	private static final String user="root";
-	private static final String pwd="welcome";
+	private static final String user="admin";
+	private static final String pwd="Kshitij@123";
 	public static void main(String ar[])
 	{
 		try{
