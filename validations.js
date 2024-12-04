2d4c4f956b14dd2c646c9a2a03a2f085fdc1ddff
Kshitij verma <kshitijkumar2702@gmail.com>
2024-12-04 23:50:00 +0530

    Add files via upload

---
+++ validations.js
@@ -0,0 +1,9 @@
+document.getElementById("loginForm").onsubmit = function(event) {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username == "" || password == "") {
+        alert("Both fields are required!");
+        event.preventDefault();  // Prevent form submission
+    }
+};
