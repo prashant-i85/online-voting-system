4343ffec5efc736c43d4732e66ea5ffab1f0feb7
Kshitij verma <kshitijkumar2702@gmail.com>
2024-12-08 21:05:35 +0530

    Delete validations.js

--- validations.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("loginForm").onsubmit = function(event) {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-
-    if (username == "" || password == "") {
-        alert("Both fields are required!");
-        event.preventDefault();  // Prevent form submission
-    }
-};
