c0ea2ff2f989fcfd1700bf35ffc09c2cf61e284a
Prashant Maurya <sunymaurya1613@gmail.com>
2024-12-30 02:37:13 +0530

    Create VoteDAO.java

---
+++ java/dao/VoteDAO.java
@@ -0,0 +1,12 @@
+package dao;
+
+import model.Vote;
+import connect.DBConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class VoteDAO {
+    // Add methods to handle vote-specific data operations
+}
