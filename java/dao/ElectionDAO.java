d9bafe487e3b3a8df6a7bd1fc774d99ce14228d4
Prashant Maurya <sunymaurya1613@gmail.com>
2024-12-30 02:36:35 +0530

    Create ElectionDAO.java

---
+++ java/dao/ElectionDAO.java
@@ -0,0 +1,12 @@
+package dao;
+
+import model.Election;
+import connect.DBConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ElectionDAO {
+    // Add methods to handle election-specific data operations
+}
