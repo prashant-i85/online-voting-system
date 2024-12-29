f70436144536e9f9c602ffa0f1e9fe95df31458e
Prashant Maurya <sunymaurya1613@gmail.com>
2024-12-30 01:44:33 +0530

    Create Candidate.java

---
+++ java/model/Candidate.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Candidate {
+    private int id;
+    private String name;
+    private String party;
+
+    public Candidate(int id, String name, String party) {
+        this.id = id;
+        this.name = name;
+        this.party = party;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getParty() {
+        return party;
+    }
+}
