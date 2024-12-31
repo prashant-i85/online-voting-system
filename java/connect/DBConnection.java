3b0228d41b2be689063e53e0d1d2381b1138a3ff
Prashant Maurya <sunymaurya1613@gmail.com>
2024-12-31 16:56:52 +0530

    Update DBConnection.java

--- java/connect/DBConnection.java
+++ java/connect/DBConnection.java
@@ -2,16 +2,19 @@ package connect;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
-    public static Connection getConnection() {
-        Connection conn = null;
+    private static final String URL = "jdbc:mysql://localhost:3306/online_voting_system";
+    private static final String USER = "root";
+    private static final String PASSWORD = "password";
+
+    public static Connection getConnection() throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/online_voting", "root", "password");
-        } catch (Exception e) {
-            e.printStackTrace();
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Database Driver not found!", e);
         }
-        return conn;
     }
 }
