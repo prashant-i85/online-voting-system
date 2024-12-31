2ffc7eb33a7ad1973f19f8bfdbde44aa44ba325c
Prashant Maurya <sunymaurya1613@gmail.com>
2024-12-31 16:59:05 +0530

    Update LoginServlet.java

--- java/servlets/LoginServlet.java
+++ java/servlets/LoginServlet.java
@@ -1,33 +1,26 @@
 package servlets;
 
 import dao.VoterDAO;
-import dao.CandidateDAO;
 import model.Voter;
-import model.Candidate;
 
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
-import java.util.List;
 
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String action = request.getParameter("action");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
 
-        if ("login".equals(action)) {
-            String email = request.getParameter("email");
-            String password = request.getParameter("password");
+        VoterDAO voterDAO = new VoterDAO();
+        Voter voter = voterDAO.authenticateVoter(email, password);
 
-            VoterDAO voterDAO = new VoterDAO();
-            Voter voter = voterDAO.login(email, password);
-
-            if (voter != null) {
-                HttpSession session = request.getSession();
-                session.setAttribute("voter", voter);
-                response.sendRedirect("voting.jsp");
-            } else {
-                response.sendRedirect("login.jsp?error=Invalid credentials");
-            }
+        if (voter != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("voter", voter);
+            response.sendRedirect("voting.jsp");
+        } else {
+            response.sendRedirect("login.jsp?error=true");
         }
     }
 }
