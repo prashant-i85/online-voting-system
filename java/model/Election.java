3b3df540a3393680e6268c1d5d681ae14b65eb7e
Prashant Maurya <sunymaurya1613@gmail.com>
2024-12-31 17:07:34 +0530

    Update Election.java

--- java/model/Election.java
+++ java/model/Election.java
@@ -1,9 +1,59 @@
 package model;
 
 public class Election {
-    private int id;
-    private String name;
+    private int electionId;
+    private String title;
     private String description;
+    private String startDate;
+    private String endDate;
 
-    // Getters and Setters
+    public Election() {}
+
+    public Election(int electionId, String title, String description, String startDate, String endDate) {
+        this.electionId = electionId;
+        this.title = title;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public int getElectionId() {
+        return electionId;
+    }
+
+    public void setElectionId(int electionId) {
+        this.electionId = electionId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
 }
