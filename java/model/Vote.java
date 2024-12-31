2f10c7beba42e1abb6513d01ce8f4ab4f6646cdd
Prashant Maurya <sunymaurya1613@gmail.com>
2024-12-31 17:08:24 +0530

    Update Vote.java

--- java/model/Vote.java
+++ java/model/Vote.java
@@ -1,10 +1,49 @@
 package model;
 
 public class Vote {
-    private int id;
+    private int voteId;
     private int voterId;
     private int candidateId;
-    private String votedAt;
+    private int electionId;
 
-    // Getters and Setters
+    public Vote() {}
+
+    public Vote(int voteId, int voterId, int candidateId, int electionId) {
+        this.voteId = voteId;
+        this.voterId = voterId;
+        this.candidateId = candidateId;
+        this.electionId = electionId;
+    }
+
+    public int getVoteId() {
+        return voteId;
+    }
+
+    public void setVoteId(int voteId) {
+        this.voteId = voteId;
+    }
+
+    public int getVoterId() {
+        return voterId;
+    }
+
+    public void setVoterId(int voterId) {
+        this.voterId = voterId;
+    }
+
+    public int getCandidateId() {
+        return candidateId;
+    }
+
+    public void setCandidateId(int candidateId) {
+        this.candidateId = candidateId;
+    }
+
+    public int getElectionId() {
+        return electionId;
+    }
+
+    public void setElectionId(int electionId) {
+        this.electionId = electionId;
+    }
 }
